8fd291ee461a4de134025b460f24f1edb24fc126
AyraAhmed <ayraahmed04@gmail.com>
2026-04-09 09:11:08 +0100

    Fixed the imports

--- frontend/src/components/dashboards/AddPatient.tsx
+++ frontend/src/components/dashboards/AddPatient.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { createPatientWithSession } from '../../services/supabaseTherapistService';
-import '../../components/dashboards/TherapistDashboard.css';
+import './TherapistDashboard.css';
 
 /**
  * Allows authorised therapists to register new patients and automatically
